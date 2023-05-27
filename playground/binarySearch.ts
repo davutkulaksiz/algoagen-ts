cd0dae41d37eb4058b8fa0b457b511f5e7749c4a
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-27 14:11:05 +0300

    feat: implement the Binary Search algorithm

--- playground/binarySearch.ts
+++ playground/binarySearch.ts
@@ -5,3 +5,46 @@
 // depending on if the half of the array is less or more than the value we look
 
 // IF THE INPUT HALVES AT EACH STEP, IT'S LIKELY O(logN) or O(NlogN)
+
+/*
+search(arr, low, high, needle) [0 ..................... N]
+
+do {
+    middle = low + (high - low) / 2
+    value = arr[middle]
+
+    if value = needle
+        return true
+
+    else if needle > value:
+        low = middle + 1
+
+    else
+        high = middle
+} while (low < high)
+return false
+*/
+
+// REMEMBER LOW IS ALWAYS INCLUSIVE HIGH IS ALWAYS EXCLUSIVE
+// [LOW, HIGH)
+
+// Array is sorted
+
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const middle = Math.floor(low + (high - low) / 2);
+        const value = haystack[middle];
+
+        if (value === needle) {
+            return true;
+        } else if (needle > value) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    } while (low < high);
+    return false;
+}
