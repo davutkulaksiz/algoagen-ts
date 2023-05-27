d467f18ede275778977db25eb5245280b0edae6c
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-27 11:56:39 +0300

    feat: implement linear search

--- playground/linearSearch.ts
+++ playground/linearSearch.ts
@@ -2,3 +2,15 @@
 // search(arr, value)
 // we'll be implementing indexOf() in a sense
 // O(N)
+
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
+}
