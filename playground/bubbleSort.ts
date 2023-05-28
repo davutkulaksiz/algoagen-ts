0f1708732ce6686e822b789d311a5626d138c553
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-28 13:34:00 +0300

    feat(sort): implement bubble sort

--- playground/bubbleSort.ts
+++ playground/bubbleSort.ts
@@ -1,3 +1,6 @@
+// Extremely Simple
+// O(N^2)
+
 export default function bubble_sort(arr: number[]): void {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - 1 - i; j++) {
