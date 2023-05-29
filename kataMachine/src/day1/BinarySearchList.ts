b3c467ee2d3b648fb8b5a7d74486d57b61badf1e
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-29 13:54:04 +0300

    refactor(kataMachine): use more meaningful variable names

--- kataMachine/src/day1/BinarySearchList.ts
+++ kataMachine/src/day1/BinarySearchList.ts
@@ -4,11 +4,11 @@ export default function bs_list(haystack: number[], needle: number): boolean {
 
     do {
         const middle = Math.floor(low + (high - low) / 2);
-        const value = haystack[middle];
+        const currentValue = haystack[middle];
 
-        if (value === needle) {
+        if (needle === currentValue) {
             return true;
-        } else if (needle > value) {
+        } else if (needle > currentValue) {
             low = middle + 1;
         } else {
             high = middle;
