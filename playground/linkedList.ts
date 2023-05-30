127c46fa506de3057320a25672f1632715d94542
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-30 12:10:57 +0300

    fix(): improve readibility

---
+++ playground/linkedList.ts
@@ -0,0 +1,9 @@
+// (A) -> (B) -> (C) -> (D)     node<T>
+//  ^                           val: T
+//  | (head)                    next?: node<T>
+// get(i)
+
+// Singly Linked List (A points B, B points C, C points D)
+// you can't walk backwards
+
+// double linked list comes with additional proprety: prev?: node<T>
